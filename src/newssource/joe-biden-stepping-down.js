3bbe69c34fb44b437588d3649e6321f378112e58
fuhktong <dustint505@gmail.com>
2024-07-11 16:33:10 +0700

    edit contact page

--- src/newssource/joe-biden-stepping-down.js
+++ src/newssource/joe-biden-stepping-down.js
@@ -1,31 +1,52 @@
 import React from 'react';
 import './globalvideo.css';
+import { Helmet } from 'react-helmet-async';
 
 const News001 = () => {
   return (
-    <div className="video-main">
-      <div className="video-space">
-      </div>
-      <div className="video-title">
-        <h1>If you want Joe Biden to step down, you need a party to do it
-        </h1>
-      </div>
-      <div className="video-date">
-        <p>11 Jul 2024</p>
-      </div>
-      <div className="news-img">
-        <img src='/joe biden stepping down.jpeg' alt="Joe Biden stepping down - courtesy Gerald Herbert - AP" />
-      </div>
-      <div className="video-news-text">
-        <p>Political parties are weak in America. Nothing is more evident of this than the two candidates for president. The unpopularity of both should motivate Democrats and Republicans to find someone with better approval ratings to win in November. However, there is nothing that senators, representatives, governors, and so on, can do to force the candidates to step down and replace them with someone with broader appeal.</p>
-        <p>Ultimately, only the candidates themselves wield the sole power to decide the country’s future. There is an inner circle like advisors, campaign staff, and even family, but outside a handful of people that may have their ear, America lacks the party system with the power to decide a party’s nominee. </p>
-        <p>Party nominations run through the primary system and are chosen by the voters in those primaries. The primary calendar is complete and both parties effectively have their nominees regardless of whether one or both nominees have a serious cognitive health issue. </p>
-        <p>We are stuck in a catch-22 where only the candidates can decide the party’s ticket while the candidate may be so mentally unsound they’re unaware they shouldn’t be on the party’s ticket. </p>
-        <p>This is an inflexible system. We don’t need cognitive tests to determine fitness. Peer review by a small group of party leaders is much more effective in determining fitness. </p>
-        <p>Clearly, there is a problem with the party primary system as Joe Biden’s 14 million voters put themselves in this difficult position (granted, there were not a lot of options).</p>
-        <p>In most of the world, parties are a small collection of members with a vested interest in the party’s direction. They decide what policies to pursue and what nominees should be given to the voters in the hope of capturing office. This is not the case in America. Here, elections decide the nominees. Populist rhetoric, popular names, media preference (or even media aversion), money, and more play a larger influence in primary elections.</p>
-        <p>With the conventions approaching, there lies a golden opportunity. If, for some reason, one or both candidates stepped down, the party convention could choose a nominee for the first time since 1968. This would be a good thing. Although this is often referred to as a floor fight in the media, we should treat an open floor convention as the ideal democratic process to choose nominees and even the president. With a few thousand delegates convening, populism, media influence, and money are no longer effective. Hopeful nominees would need negotiation and coalition-building skills to sway convention delegates.</p>
-        <p>The prospect of the Democratic Party forcing Biden to step down is near zero. It’s just not how our system works. But if it were to occur, for a brief few days, the Democrats would have a functioning party system. If this happened, it might inspire Americans to demand a better way to pick their party’s nominees.</p>
+    <div>
+      <Helmet>
+        <title>The Framers' Method - News</title>
+        <meta name="description" content="If you want Joe Biden to step down, you need a party to do it" />
+        <meta name="keywords" content="joe biden, electoral college, presidential elections, american politics, electors, republic, president, constitution, democracy" />
+        <meta name="author" content="Dustin Taylor" />
+        <meta name="robots" content="index, follow"></meta>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8"></meta>
+        <meta name="language" content="English"></meta>
+        <meta property="og:title" content="The Framers' Method - News" />
+        <meta property="og:description" content="If you want Joe Biden to step down, you need a party to do it." />
+        <meta property="og:image" content="https://www.framersmethod.com/joe biden stepping down.jpeg" />
+        <meta property="og:url" content="https://www.framersmethod.com" />
+        <meta property="og:type" content="website" />
+        <meta name="twitter:title" content="The Framers' Method - News" />
+        <meta name="twitter:description" content="If you want Joe Biden to step down, you need a party to do it" />
+        <meta name="twitter:image" content="https://www.framersmethod.com/joe biden stepping down.jpeg" />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Helmet>
+      <div className="video-main">
+        <div className="video-space">
+        </div>
+        <div className="video-title">
+          <h1>If you want Joe Biden to step down, you need a party to do it
+          </h1>
+        </div>
+        <div className="video-date">
+          <p>11 Jul 2024</p>
+        </div>
+        <div className="news-img">
+          <img src='/joe biden stepping down.jpeg' alt="Joe Biden stepping down - courtesy Gerald Herbert - AP" />
+        </div>
+        <div className="video-news-text">
+          <p>Political parties are weak in America. Nothing is more evident of this than the two candidates for president. The unpopularity of both should motivate Democrats and Republicans to find someone with better approval ratings to win in November. However, there is nothing that senators, representatives, governors, and so on, can do to force the candidates to step down and replace them with someone with broader appeal.</p>
+          <p>Ultimately, only the candidates themselves wield the sole power to decide the country’s future. There is an inner circle like advisors, campaign staff, and even family, but outside a handful of people that may have their ear, America lacks the party system with the power to decide a party’s nominee. </p>
+          <p>Party nominations run through the primary system and are chosen by the voters in those primaries. The primary calendar is complete and both parties effectively have their nominees regardless of whether one or both nominees have a serious cognitive health issue. </p>
+          <p>We are stuck in a catch-22 where only the candidates can decide the party’s ticket while the candidate may be so mentally unsound they’re unaware they shouldn’t be on the party’s ticket. </p>
+          <p>This is an inflexible system. We don’t need cognitive tests to determine fitness. Peer review by a small group of party leaders is much more effective in determining fitness. </p>
+          <p>Clearly, there is a problem with the party primary system as Joe Biden’s 14 million voters put themselves in this difficult position (granted, there were not a lot of options).</p>
+          <p>In most of the world, parties are a small collection of members with a vested interest in the party’s direction. They decide what policies to pursue and what nominees should be given to the voters in the hope of capturing office. This is not the case in America. Here, elections decide the nominees. Populist rhetoric, popular names, media preference (or even media aversion), money, and more play a larger influence in primary elections.</p>
+          <p>With the conventions approaching, there lies a golden opportunity. If, for some reason, one or both candidates stepped down, the party convention could choose a nominee for the first time since 1968. This would be a good thing. Although this is often referred to as a floor fight in the media, we should treat an open floor convention as the ideal democratic process to choose nominees and even the president. With a few thousand delegates convening, populism, media influence, and money are no longer effective. Hopeful nominees would need negotiation and coalition-building skills to sway convention delegates.</p>
+          <p>The prospect of the Democratic Party forcing Biden to step down is near zero. It’s just not how our system works. But if it were to occur, for a brief few days, the Democrats would have a functioning party system. If this happened, it might inspire Americans to demand a better way to pick their party’s nominees.</p>
+        </div>
       </div>
     </div>
   );
