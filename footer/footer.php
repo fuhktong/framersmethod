3194894a5b7368ea9b17895fad7c9fdd21af7a41
fuhktong <dustint505@gmail.com>
2025-07-03 14:07:05 +0700

    Add GitHub Actions deployment

--- footer/footer.php
+++ footer/footer.php
@@ -40,25 +40,25 @@
         </div>
         <div class="footer-social-media">
             <div class="social-row">
-                <a href="https://www.instagram.com/framersmethod/" target="_blank" rel="noreferrer">
-                    <img src="/images/whitelogoinsta.png" alt="The Framers' Method on Instagram" />
-                </a>
                 <a href="https://bsky.app/profile/framersmethod.bsky.social" target="_blank" rel="noreferrer">
                     <img src="/images/whitelogobluesky.png" alt="The Framers' Method on Bluesky" />
                 </a>
-                <a href="https://medium.com/@framersmethod" target="_blank" rel="noreferrer">
-                    <img src="/images/whitelogomedium.png" alt="The Framers' Method - Medium" />
+                <a href="https://www.instagram.com/framersmethod/" target="_blank" rel="noreferrer">
+                    <img src="/images/whitelogoinsta.png" alt="The Framers' Method on Instagram" />
                 </a>
-                <a href="https://substack.com/@framersmethod" target="_blank" rel="noreferrer">
-                    <img src="/images/whitelogosubstack.png" alt="The Framers' Method - Substack" />
-                </a>
-            </div>
-            <div class="social-row">
                 <a href="https://www.youtube.com/@framersmethod/featured" target="_blank" rel="noreferrer">
                     <img src="/images/whitelogoyoutube.png" alt="The Framers' Method on YouTube" />
                 </a>
                 <a href="https://www.tiktok.com/@framersmethod" target="_blank" rel="noreferrer">
                     <img src="/images/whitelogotiktok.png" alt="The Framers' Method on TikTok" />
+                </a>
+            </div>
+            <div class="social-row">
+                <a href="https://medium.com/@framersmethod" target="_blank" rel="noreferrer">
+                    <img src="/images/whitelogomedium.png" alt="The Framers' Method - Medium" />
+                </a>
+                <a href="https://substack.com/@framersmethod" target="_blank" rel="noreferrer">
+                    <img src="/images/whitelogosubstack.png" alt="The Framers' Method - Substack" />
                 </a>
                 <a href="https://a.co/d/0dimzJAr" target="_blank" rel="noreferrer">
                     <img src="/images/whitelogoamazon.png" alt="On The Framers' Method Book - Amazon" />
--- socialmediabar/socialmediabar.php
+++ socialmediabar/socialmediabar.php
@@ -1,22 +1,22 @@
 <!-- Social Media Bar -->
 <section class="socialmediabar" id="socialmediabar">
+    <a href="https://bsky.app/profile/framersmethod.bsky.social" target="_blank" rel="noreferrer">
+        <img src="/images/whitelogobluesky.png" alt="The Framers' Method on Bluesky" />
+    </a>
     <a href="https://www.instagram.com/framersmethod/" target="_blank" rel="noreferrer">
         <img src="/images/whitelogoinsta.png" alt="The Framers' Method on Instagram" />
     </a>
-    <a href="https://bsky.app/profile/framersmethod.bsky.social" target="_blank" rel="noreferrer">
-        <img src="/images/whitelogobluesky.png" alt="The Framers' Method on Bluesky" />
+    <a href="https://www.tiktok.com/@framersmethod" target="_blank" rel="noreferrer">
+        <img src="/images/whitelogotiktok.png" alt="The Framers' Method on TikTok" />
+    </a>
+    <a href="https://www.youtube.com/@framersmethod/featured" target="_blank" rel="noreferrer">
+        <img src="/images/whitelogoyoutube.png" alt="The Framers' Method on YouTube" />
     </a>
     <a href="https://medium.com/@framersmethod" target="_blank" rel="noreferrer">
         <img src="/images/whitelogomedium.png" alt="The Framers' Method - Medium" />
     </a>
     <a href="https://substack.com/@framersmethod" target="_blank" rel="noreferrer">
         <img src="/images/whitelogosubstack.png" alt="The Framers' Method - Substack" />
-    </a>
-    <a href="https://www.youtube.com/@framersmethod/featured" target="_blank" rel="noreferrer">
-        <img src="/images/whitelogoyoutube.png" alt="The Framers' Method on YouTube" />
-    </a>
-    <a href="https://www.tiktok.com/@framersmethod" target="_blank" rel="noreferrer">
-        <img src="/images/whitelogotiktok.png" alt="The Framers' Method on TikTok" />
     </a>
     <a href="https://a.co/d/0dimzJAr" target="_blank" rel="noreferrer">
         <img src="/images/whitelogoamazon.png" alt="On The Framers' Method Book - Amazon" />
@@ -32,25 +32,26 @@ function updateSocialMediaBar() {
     if (isMobile) {
         socialBar.innerHTML = `
             <div class="socialmediabar-row">
-                <a href="https://www.instagram.com/framersmethod/" target="_blank" rel="noreferrer">
-                    <img src="/images/whitelogoinsta.png" alt="The Framers' Method on Instagram" />
-                </a>
                 <a href="https://bsky.app/profile/framersmethod.bsky.social" target="_blank" rel="noreferrer">
                     <img src="/images/whitelogobluesky.png" alt="The Framers' Method on Bluesky" />
                 </a>
-                <a href="https://medium.com/@framersmethod" target="_blank" rel="noreferrer">
-                    <img src="/images/whitelogomedium.png" alt="The Framers' Method - Medium" />
+                <a href="https://www.instagram.com/framersmethod/" target="_blank" rel="noreferrer">
+                    <img src="/images/whitelogoinsta.png" alt="The Framers' Method on Instagram" />
                 </a>
-                <a href="https://substack.com/@framersmethod" target="_blank" rel="noreferrer">
-                    <img src="/images/whitelogosubstack.png" alt="The Framers' Method - Substack" />
-                </a>
-            </div>
-            <div class="socialmediabar-row">
                 <a href="https://www.youtube.com/@framersmethod/featured" target="_blank" rel="noreferrer">
                     <img src="/images/whitelogoyoutube.png" alt="The Framers' Method on YouTube" />
                 </a>
                 <a href="https://www.tiktok.com/@framersmethod" target="_blank" rel="noreferrer">
                     <img src="/images/whitelogotiktok.png" alt="The Framers' Method on TikTok" />
+                </a>
+
+            </div>
+            <div class="socialmediabar-row">
+                <a href="https://medium.com/@framersmethod" target="_blank" rel="noreferrer">
+                    <img src="/images/whitelogomedium.png" alt="The Framers' Method - Medium" />
+                </a>
+                <a href="https://substack.com/@framersmethod" target="_blank" rel="noreferrer">
+                    <img src="/images/whitelogosubstack.png" alt="The Framers' Method - Substack" />
                 </a>
                 <a href="https://a.co/d/0dimzJAr" target="_blank" rel="noreferrer">
                     <img src="/images/whitelogoamazon.png" alt="On The Framers' Method Book - Amazon" />
@@ -59,27 +60,27 @@ function updateSocialMediaBar() {
         `;
     } else {
         socialBar.innerHTML = `
-            <a href="https://www.instagram.com/framersmethod/" target="_blank" rel="noreferrer">
-                <img src="/images/whitelogoinsta.png" alt="The Framers' Method on Instagram" />
-            </a>
-            <a href="https://bsky.app/profile/framersmethod.bsky.social" target="_blank" rel="noreferrer">
-                <img src="/images/whitelogobluesky.png" alt="The Framers' Method on Bluesky" />
-            </a>
-            <a href="https://medium.com/@framersmethod" target="_blank" rel="noreferrer">
-                <img src="/images/whitelogomedium.png" alt="The Framers' Method - Medium" />
-            </a>
-            <a href="https://substack.com/@framersmethod" target="_blank" rel="noreferrer">
-                <img src="/images/whitelogosubstack.png" alt="The Framers' Method - Substack" />
-            </a>
-            <a href="https://www.youtube.com/@framersmethod/featured" target="_blank" rel="noreferrer">
-                <img src="/images/whitelogoyoutube.png" alt="The Framers' Method on YouTube" />
-            </a>
-            <a href="https://www.tiktok.com/@framersmethod" target="_blank" rel="noreferrer">
-                <img src="/images/whitelogotiktok.png" alt="The Framers' Method on TikTok" />
-            </a>
-            <a href="https://a.co/d/0dimzJAr" target="_blank" rel="noreferrer">
-                <img src="/images/whitelogoamazon.png" alt="On The Framers' Method Book - Amazon" />
-            </a>
+        <a href="https://bsky.app/profile/framersmethod.bsky.social" target="_blank" rel="noreferrer">
+            <img src="/images/whitelogobluesky.png" alt="The Framers' Method on Bluesky" />
+        </a>
+        <a href="https://www.instagram.com/framersmethod/" target="_blank" rel="noreferrer">
+            <img src="/images/whitelogoinsta.png" alt="The Framers' Method on Instagram" />
+        </a>
+        <a href="https://www.tiktok.com/@framersmethod" target="_blank" rel="noreferrer">
+            <img src="/images/whitelogotiktok.png" alt="The Framers' Method on TikTok" />
+        </a>
+        <a href="https://www.youtube.com/@framersmethod/featured" target="_blank" rel="noreferrer">
+            <img src="/images/whitelogoyoutube.png" alt="The Framers' Method on YouTube" />
+        </a>
+        <a href="https://medium.com/@framersmethod" target="_blank" rel="noreferrer">
+            <img src="/images/whitelogomedium.png" alt="The Framers' Method - Medium" />
+        </a>
+        <a href="https://substack.com/@framersmethod" target="_blank" rel="noreferrer">
+            <img src="/images/whitelogosubstack.png" alt="The Framers' Method - Substack" />
+        </a>
+        <a href="https://a.co/d/0dimzJAr" target="_blank" rel="noreferrer">
+            <img src="/images/whitelogoamazon.png" alt="On The Framers' Method Book - Amazon" />
+        </a>
         `;
     }
 }
