2acda5f377ef03d9c2cdbdc22d60abd789822bc3
fuhktong <dustint505@gmail.com>
2026-02-20 15:18:33 +0700

    Add GitHub Actions deployment

--- pages/book.php
+++ pages/book.php
@@ -13,6 +13,7 @@
             <p>
                 Whatever you think about the Electoral College, it’s not what the framers designed. In these pages, you’ll discover what the framers actually created, how radically different the Electoral College is from our modern incarnation, and what reforms will bring us back to the framers’ original vision of choosing a president.
             </p>
+            <br>
             <p>
                 Drawing on original analysis and data, On the Framers’ Electoral College reveals how the delegates to the Constitutional Convention built a system of decentralization and deliberation to elect a president. By dispersing power to electors across the several states to nominate candidates, they ensured that no "designing men" could manipulate and control the masses. By returning to the framers’ method, we can protect the United States from the destructiveness of populism and tyranny.
             </p>
